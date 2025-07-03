d207312b3c5886fe3e6970e3dbab88de823893ec
Hilppah <hilppa.huhtanen@gmail.com>
2025-07-03 18:17:16 +0300

    Test database

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,13 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("Connected to MongoDB Atlas"))
+  .catch(err => console.error("MongoDB connection error:", err));
+
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
