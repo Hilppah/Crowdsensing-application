b253fb79204bcbddc31f53c667bf738e99b71f7f
Hilppah <hilppa.huhtanen@gmail.com>
2025-08-18 18:13:09 +0300

    Show data on the send page

--- application/app/src/main/java/com/crowdsensing/ApiClient.kt
+++ application/app/src/main/java/com/crowdsensing/ApiClient.kt
@@ -1,6 +1,11 @@
 package com.crowdsensing
 
-import okhttp3.*
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.FormBody
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
 import java.io.IOException
 
 class ApiClient(private val baseUrl: String) {
--- application/app/src/main/java/com/crowdsensing/NewDataFragment.kt
+++ application/app/src/main/java/com/crowdsensing/NewDataFragment.kt
@@ -6,7 +6,6 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
-import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.fragment.app.Fragment
 import com.crowdsensing.ApiClient
 import com.crowdsensing.R
