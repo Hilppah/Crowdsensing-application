fc36912953de13be2aa2512476385832018c55d1
Hilppah <hilppa.huhtanen@gmail.com>
2025-09-09 15:39:37 +0300

    save a session as a collection

--- backend/models/recordingSession.js
+++ backend/models/recordingSession.js
@@ -1,13 +1,56 @@
 const mongoose = require("mongoose");
 
-const recordingSessionSchema = new mongoose.Schema({
-  phoneModel: String,
-  startTime: Date,
-  endTime: Date,
-  description: String,
-  frequency: Number,
-  chosenMeasurement: String,
-  stability: { type: Number, default: 0 }
-}, { versionKey: false });
+const recordingSessionSchema = new mongoose.Schema(
+  {
+    phoneModel: String,
+    startTime: Date,
+    endTime: Date,
+    description: String,
+    frequency: Number,
+    chosenMeasurement: String,
+    stability: { type: Number, default: 0 },
+    gps: [
+      {
+        latitude: Number,
+        longitude: Number,
+        timestamp: Date,
+        stability: Number,
+      },
+    ],
+    compass: [
+      {
+        compassData: Number,
+        timestamp: Date,
+        stability: Number,
+      },
+    ],
+    accelerometer: [
+      {
+        accelX: Number,
+        accelY: Number,
+        accelZ: Number,
+        timestamp: Date,
+        stability: Number,
+      },
+    ],
+    gyroscope: [
+      {
+        gyroX: Number,
+        gyroY: Number,
+        gyroZ: Number,
+        timestamp: Date,
+        stability: Number,
+      },
+    ],
+    proximity: [
+      {
+        proximity: Number,
+        timestamp: Date,
+        stability: Number,
+      },
+    ],
+  },
+  { versionKey: false }
+);
 
 module.exports = mongoose.model("RecordingSession", recordingSessionSchema);
