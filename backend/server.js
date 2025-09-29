3fe359586cafb3e3ce3308881e85cbedcb374acf
Hilppah <hilppa.huhtanen@gmail.com>
2025-09-29 16:08:13 +0300

    Fix app for render

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,25 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const sessionRoutes = require("./routes/sessions");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
-
+app.use(cors());
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("Connected to MongoDB Atlas"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
-app.use(express.json());
 app.use("/api/sessions", sessionRoutes);
-
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Sensor Data API");
 });
 
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on port ${port}`);
 });
