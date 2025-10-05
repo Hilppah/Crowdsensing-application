d35b348f6a88a8707c54adf9e1f37a80166bffb2
Hilppah <hilppa.huhtanen@gmail.com>
2025-10-05 18:28:49 +0300

    fix typo

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const sessionRoutes = require("./routes/sessions");
 const app = express();
 const port = process.env.PORT || 3000;
 
-pp.use(express.json({ limit: "100mb" }));
+app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ limit: "100mb", extended: true }));
 app.use(cors());
 mongoose
